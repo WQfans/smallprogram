58bf5faa1e5e48425b0a06e15e5b1978ce677303
shishu <1042553645@qq.com>
2019-08-04 22:00:20 +0800

    bug
    没有授权时授权的bug

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -109,5 +109,13 @@ Page({
         // })
       }
     })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
