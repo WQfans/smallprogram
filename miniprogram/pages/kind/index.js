acbc8b20030e7652d0373676451919db5f47046b
shishu <1042553645@qq.com>
2019-08-05 22:42:01 +0800

    tab
    tab

--- miniprogram/pages/kind/index.js
+++ miniprogram/pages/kind/index.js
@@ -1,11 +1,34 @@
 const weuiminiprogram = require('weui-miniprogram');
-Component({
+Page({
   data: {
-    selectedIndex:0,
+    selectedIndex: 0,
+    height: '400px',
     kind: [{
       title: '基础护理',
     }, {
       title: '面膜',
+    }, {
+      title: '洁面',
+    }, {
+      title: '卸妆',
+    }, {
+      title: '防晒',
+    }, {
+      title: '底妆',
+    }, {
+      title: '彩妆',
+    }, {
+      title: '美甲',
+    }, {
+      title: '护理',
+    }, {
+      title: '男士',
+    }, {
+      title: '工具',
+    }, {
+      title: '优惠套装',
+    }, {
+      title: '限量产品',
     }]
   },
   pageLifetimes: {
@@ -17,5 +40,25 @@ Component({
         })
       }
     }
+  },
+  onLoad: function () {
+    this.getScreenHeight();
+  },
+  getScreenHeight() {
+    wx.getSystemInfo({
+      success: res => {
+        console.log(res.windowHeight)
+        this.setData({
+          height: res.windowHeight -48 + 'px'
+        })
+      }
+    })
+  },
+
+  switchKind(e) {
+    const data = e.currentTarget.dataset
+    this.setData({
+      selectedIndex: data.index
+    })
   }
 })
