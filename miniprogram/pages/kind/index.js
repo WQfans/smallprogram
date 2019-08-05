5edce613800e1d8f1efcf04d4ae70ef3b2fbbaac
shishu <1042553645@qq.com>
2019-08-05 21:50:49 +0800

    组件库
    组件库

--- miniprogram/pages/kind/index.js
+++ miniprogram/pages/kind/index.js
@@ -1,5 +1,13 @@
-
+const weuiminiprogram = require('weui-miniprogram');
 Component({
+  data: {
+    selectedIndex:0,
+    kind: [{
+      title: '基础护理',
+    }, {
+      title: '面膜',
+    }]
+  },
   pageLifetimes: {
     show() {
       if (typeof this.getTabBar === 'function' &&
