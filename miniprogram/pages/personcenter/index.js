b1abbe668fa8d70e5bab9006dafadb29963f94d7
ciayomin <ciaymxx327@gmail.com>
2019-08-07 22:10:41 +0800

    个人中心
    个人中心

--- miniprogram/pages/personcenter/index.js
+++ miniprogram/pages/personcenter/index.js
@@ -1,13 +1,65 @@
-
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 0
+Page({
+  data: {
+    grids: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  },
+  onLoad: function () {
+    // 获取用户信息
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      console.log(app.globalData.userInfo)
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
         })
+        console.log(res.userInfo)
       }
     }
+    this.getOpenid();
+    this.getVipInfo();
+  },
+  getOpenid: function () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../personcenter/index',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+  getVipInfo: function () {
+    wx.cloud.callFunction({
+      name: 'getVipInfo',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result)
+        this.setData({
+          vipInfo: res.result.data[0].vipInfo
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
   }
-})
+});
