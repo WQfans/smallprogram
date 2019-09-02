ad1d3c1fdced8e97e3f0e9faefd9c918ab4d09b1
shishu <1042553645@qq.com>
2019-09-02 23:06:41 +0800

    ss
    ss

--- miniprogram/pages/personcenter/index.js
+++ miniprogram/pages/personcenter/index.js
@@ -2,10 +2,30 @@
 const app = getApp()
 Page({
   data: {
+    userInfo:{},
     height: '600px',
+    hasUserInfo: false,
   },
   onLoad: function () {
     this.getScreenHeight();
+    // 获取用户信息
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      console.log(app.globalData.userInfo)
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        console.log(res.userInfo)
+      }
+    }
   },
   getScreenHeight() {
       wx.getSystemInfo({
