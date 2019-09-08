6facc4fb6163b25e5b09b78e0dc63e3bdb93d56f
shishu <1042553645@qq.com>
2019-09-08 23:02:22 +0800

    ss
    ss

--- miniprogram/pages/personlocation/index.js
+++ miniprogram/pages/personlocation/index.js
@@ -1,33 +1,76 @@
 const app = getApp()
 
 Page({
-  chooseAddress(){
+  data: {
+    userInfo:[]
+  },
+  pageLifetimes: {
+  },
+  onLoad: function (options) {
+    this.getUserLocation()
+  },
+  chooseAddress() {
     console.log(app.globalData.userInfo)
-    let saveUserInfo = this.saveUserInfo;
+    let saveUserLocation = this.saveUserLocation;
+    let addLocationInfo = this.addLocationInfo;
     wx.chooseAddress({
       success(res) {
-        console.log(res.userName)
-        console.log(res.postalCode)
-        console.log(res.provinceName)
-        console.log(res.cityName)
-        console.log(res.countyName)
-        console.log(res.detailInfo)
-        console.log(res.nationalCode)
-        console.log(res.telNumber)
-        let { userName, postalCode, provinceName, cityName, countyName, detailInfo, nationalCode, telNumber} = res;
-        let userInfo = { userName, postalCode, provinceName, cityName, countyName, detailInfo, nationalCode, telNumber }
-        saveUserInfo(userInfo)
+        let {
+          userName,
+          postalCode,
+          provinceName,
+          cityName,
+          countyName,
+          detailInfo,
+          nationalCode,
+          telNumber
+        } = res;
+        let userInfo = {
+          userName,
+          postalCode,
+          provinceName,
+          cityName,
+          countyName,
+          detailInfo,
+          nationalCode,
+          telNumber
+        }
+        addLocationInfo(userInfo)
+        saveUserLocation(userInfo)
       }
     })
   },
-  saveUserInfo(userInfo){
+  getUserLocation() {
     wx.cloud.callFunction({
-      name: 'savePersonInfo',
-      data: userInfo,
+      name: 'getpersonlocation',
       success: res => {
+        console.log(res);
+        this.setData({
+          userInfo: res.result.data
+        })
       },
       fail: err => {
+        console.log(err)
       }
     })
   },
+  saveUserLocation(userInfo) {
+    wx.cloud.callFunction({
+      name: 'savePersonLocation', 
+      data: userInfo,
+      success: res => {
+        console.log(res);
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
+  },
+  addLocationInfo(userInfo){
+    let allInfo = this.data.userInfo;
+    allInfo.push(userInfo);
+    this.setData({
+      userInfo: allInfo
+    })
+  }
 });
