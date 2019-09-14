22a4977ff7e82dfa7834e7b183f37cd41c26dffb
shishu <1042553645@qq.com>
2019-09-14 19:34:49 +0800

    ss
    ss

--- miniprogram/pages/personlocation/index.js
+++ miniprogram/pages/personlocation/index.js
@@ -4,13 +4,23 @@ Page({
   data: {
     userInfo:[],
     displayLocation:false,
-    height: '500px'
+    height: '500px',
+    calcScrollLeft:{},
+    delDialogShow:false,
+    buttons: [{ text: '取消' }, { text: '确定' }],
   },
   pageLifetimes: {
   },
   onLoad: function (options) {
     this.getUserLocation();
     this.getScreenHeight();
+  },
+  refreshLocation(e){
+    let calcScrollLeft = {...this.data.calcScrollLeft}
+    calcScrollLeft[e.currentTarget.dataset.index] = '0'
+    this.setData({
+      calcScrollLeft: calcScrollLeft
+    })
   },
   getScreenHeight() {
     wx.getSystemInfo({
@@ -22,7 +32,6 @@ Page({
     })
   },
   chooseAddress() {
-    console.log(app.globalData.userInfo)
     let saveUserLocation = this.saveUserLocation;
     let addLocationInfo = this.addLocationInfo;
     wx.chooseAddress({
@@ -90,5 +99,43 @@ Page({
     this.setData({
       userInfo: allInfo
     })
+  },
+  delLocation(e){
+    console.log(e)
+    wx.cloud.callFunction({
+      name: 'delpersonLocation',
+      data:{
+        id: e.currentTarget.dataset.id
+      },
+      success: res => {
+        console.log(res);
+        let newUserInfo = [];
+        let userInfo = this.data.userInfo;
+        for (let i = 0; i < userInfo.length;i++){
+          if (userInfo._id != e.currentTarget.dataset.id){
+            newUserInfo.push(userInfo[i])
+          }
+        }
+        this.setData({
+          userInfo:newUserInfo
+        })
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
+  },
+  delDialogOnShow: function () {
+    this.setData({
+      delDialogShow: true
+    })
+  },
+  tapDelDialogButton(e){
+    this.setData({
+      delDialogShow:false
+    })
+    if(e.detail.index==1){
+      this.delLocation(e)
+    }
   }
 });
