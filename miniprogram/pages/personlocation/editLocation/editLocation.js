74ada5d89a28134e35bb9134be7fe28d18bc1e91
shishu <1042553645@qq.com>
2019-09-15 22:52:01 +0800

    新增地址
    新增地址

--- miniprogram/pages/personlocation/editLocation/editLocation.js
+++ miniprogram/pages/personlocation/editLocation/editLocation.js
@@ -4,5 +4,11 @@ Page({
   },
   onLoad: function (options) {
     // 页面创建时执行
-  }
+    wx.setBackgroundColor({
+      backgroundColor: '#000', // 窗口的背景色为白色
+    })
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
 })
